6d51ffa88502e4c2dd88a2a197e9da118c43a78f
enjoythecode <sinan.yumurtaci@gmail.com>
2020-10-28 19:12:20 -0400

    translation speed, move logic around a little

--- Assets/Landmarks/Scripts/ExperimentTasks/LM_ObjectPlacement.cs
+++ Assets/Landmarks/Scripts/ExperimentTasks/LM_ObjectPlacement.cs
@@ -55,75 +55,80 @@ public class LM_ObjectPlacement : ExperimentTask
 
     public override bool updateTask()
     {
-
-        // WRITE TASK UPDATE CODE HERE
-
+    
         // During the orienting stage
-        if ( Input.GetKeyDown(KeyCode.Return) && !oriented)
-        {
+        if (!oriented){
+            
+            if ( Input.GetKeyDown(KeyCode.Return))
+            {
+                if (vrEnabled)
+                {
+                    Debug.Log("todo--objectPlacement--VR");
+                }
+                else 
+                {
+                    // Lock player movement & reset looking at the current orientation
+                    avatar.GetComponent<FirstPersonController>().enabled = false; // disable the controller to work
+                    //avatar.GetComponentInChildren<Camera>().transform.localEulerAngles = new Vector3 (25, 0, 0); // reset the camera
+                    //avatar.GetComponent<FirstPersonController>().ResetMouselook(); // reset the zero position to be our current cam orientation
+                }
+
+                // Instantiate the Marker
+                markerLocation = manager.player.transform.position + avatar.GetComponentInChildren<Camera>().transform.forward * markerStartDistance;
+                markerLocation.y = markerFixedHeight;
+                markerObject = Instantiate(markerObjectTemplate, markerLocation, Quaternion.identity);
+                //markerObject.transform.localPosition = new Vector3(0,0, -markerStartDistance);    
+                //markerObject.transform.localEulerAngles = Vector3.zero;
+
+                // label them as oriented
+                oriented = true;
+            }
+        }
+
+        // During the pointing stage (after orienting)
+        else{ // if (oriented)
+
+            // check for key updates
+            // and move markerLocation accordingly
+            // and update the markerObject.Transform
+
             if (vrEnabled)
             {
-
+                Debug.Log("todo--objectPointing--VR");
             }
-            else 
+            else
             {
-                // Lock player movment & reset looking
-                avatar.GetComponent<FirstPersonController>().enabled = false; // disable the controller to work
-                //avatar.GetComponentInChildren<Camera>().transform.localEulerAngles = new Vector3 (25, 0, 0); // reset the camera
-                avatar.GetComponent<FirstPersonController>().ResetMouselook(); // reset the zero position to be our current cam orientation
+                int targetMovementHori = 0;
+                int targetMovementVert = 0;
+                Vector3 targetTranslation = Vector3.zero;
+                if(Input.GetKey(KeyCode.U)){
+                    targetMovementVert = 1;
+                }
+                if (Input.GetKey(KeyCode.M)){
+                    targetMovementVert = -1;
+                }
+                if (Input.GetKey(KeyCode.H)){
+                    targetMovementHori = -1;
+                }
+                if (Input.GetKey(KeyCode.K)){
+                    targetMovementHori = 1;  
+                }
+                
+                if (targetMovementHori != 0){
+                    targetTranslation += avatar.GetComponentInChildren<Camera>().transform.right * Time.deltaTime * targetMovementHori;
+                }
+                if (targetMovementVert != 0){
+                    targetTranslation += avatar.GetComponentInChildren<Camera>().transform.forward * Time.deltaTime * targetMovementVert;
+                    Debug.Log(targetTranslation);
+                }
+                markerLocation = markerObject.transform.position;
+                markerLocation += targetTranslation * translationSpeed;
+                markerLocation.y = markerFixedHeight;
+                markerObject.transform.position = markerLocation;
+                
             }
-
-            // Instantiate the Marker
-            markerLocation = manager.player.transform.position + avatar.GetComponentInChildren<Camera>().transform.forward * markerStartDistance;
-            markerLocation.y = markerFixedHeight;
-            markerObject = Instantiate(markerObjectTemplate, markerLocation, Quaternion.identity);
-            //markerObject.transform.localPosition = new Vector3(0,0, -markerStartDistance);    
-            //markerObject.transform.localEulerAngles = Vector3.zero;
-
-            // label them as oriented
-            oriented = true;
         }
 
-        // check for key updates
-        // and move markerLocation accordingly
-        // and update the markerObject.Transform
-
-        if (vrEnabled)
-        {
-            Debug.Log("todo");
-        }
-        else
-        {
-            int targetMovementHori = 0;
-            int targetMovementVert = 0;
-            Vector3 targetTranslation = Vector3.zero;
-            if(Input.GetKey(KeyCode.U)){
-                targetMovementVert = 1;
-            }
-            if (Input.GetKey(KeyCode.M)){
-                targetMovementVert = -1;
-            }
-            if (Input.GetKey(KeyCode.H)){
-                targetMovementHori = -1;
-            }
-            if (Input.GetKey(KeyCode.K)){
-                targetMovementHori = 1;  
-            }
-            
-            if (targetMovementHori != 0){
-                targetTranslation += avatar.GetComponentInChildren<Camera>().transform.right * Time.deltaTime * targetMovementHori;
-            }
-            if (targetMovementVert != 0){
-                targetTranslation += avatar.GetComponentInChildren<Camera>().transform.forward * Time.deltaTime * targetMovementVert;
-                Debug.Log(targetTranslation);
-            }
-            markerLocation = markerObject.transform.position;
-            markerLocation += targetTranslation;
-            markerLocation.y = markerFixedHeight;
-            markerObject.transform.position = markerLocation;
-            
-
-        }
         return false;
         
     }
