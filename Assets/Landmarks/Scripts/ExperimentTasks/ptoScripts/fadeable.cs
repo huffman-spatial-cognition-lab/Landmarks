ac79e209f99703ac2b64f4f5f7b3bee5613b7015
Derek Huffman <derek.huffman23@gmail.com>
2024-02-24 09:56:49 -0500

    Fixed fadeIn for boundaries/door; now need to fix walls and fade in of objects (DJH 2024 FEB 24)

--- Assets/Landmarks/Scripts/ExperimentTasks/ptoScripts/fadeable.cs
+++ Assets/Landmarks/Scripts/ExperimentTasks/ptoScripts/fadeable.cs
@@ -18,50 +18,72 @@ public class fadeable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mat = this.GetComponent<Renderer>().material;
-        float colorDirection;
-
-        colorDirection = 0;
-        if(fadeOut)
+        // DJH turning this into an if statement, since we don't need to run this if not changing alpha/color
+        if (fadeIn || fadeOut)
         {
-            colorDirection = -1;
-        }
+            mat = this.GetComponent<Renderer>().material;
+            float colorDirection;
 
-        if(fadeIn)
-        {
-            colorDirection = 1;
-        }
-
-        Color objectColor = mat.color;
-        float newAlpha = objectColor.a + (fadeSpeed * Time.deltaTime * colorDirection);
-
-        Color newColor = new Color(objectColor.r, objectColor.g, objectColor.b, newAlpha);
-        mat.color = newColor;
-        
-        if(fadeOut && newAlpha <= 0)
-        {
-            fadeOut = false;
-            if(destroyAfterFadeOut)
+            colorDirection = 0;
+            if (fadeOut)
             {
-                Destroy(this.transform.gameObject);
+                colorDirection = -1;
             }
-            if(afterTransitionMaterial)
+
+            if (fadeIn)
             {
-                this.transform.gameObject.GetComponent<Renderer>().material = afterTransitionMaterial;
-                afterTransitionMaterial = null;
+                colorDirection = 1;
             }
-            if(disableAfterFadeOut)
+
+            Color objectColor = mat.color;
+            float newAlpha = objectColor.a + (fadeSpeed * Time.deltaTime * colorDirection);
+
+            //if (fadeIn && notIncreasedBrightnessYet)
+            //{
+            //    newAlpha = 0.0f + (fadeSpeed * Time.deltaTime * colorDirection);
+            //    notIncreasedBrightnessYet = false;
+            //}
+
+            Debug.Log("GAMEOBJECT");
+            Debug.Log(this.gameObject.name);
+            Debug.Log("COLORDIRECTION");
+            Debug.Log(colorDirection);
+            Debug.Log("NEWALPHA");
+            Debug.Log(newAlpha);
+
+            Color newColor = new Color(objectColor.r, objectColor.g, objectColor.b, newAlpha);
+            mat.color = newColor;
+
+            if (fadeOut && newAlpha <= 0)
             {
-                this.transform.gameObject.SetActive(false);
-                disableAfterFadeOut = false;
+                Debug.Log("WE ARE FADING OUT HERE!!!");
+                Debug.Log(this.transform.gameObject.name);
+                fadeOut = false;
+                if (destroyAfterFadeOut)
+                {
+                    Destroy(this.transform.gameObject);
+                }
+                if (afterTransitionMaterial)
+                {
+                    this.transform.gameObject.GetComponent<Renderer>().material = afterTransitionMaterial;
+                    afterTransitionMaterial = null;
+                }
+                if (disableAfterFadeOut)
+                {
+                    this.transform.gameObject.SetActive(false);
+                    disableAfterFadeOut = false;
+                }
+            }
+
+            if (fadeIn && newAlpha >= 1)
+            {
+                fadeIn = false;
+                // DJH edits
+                //notIncreasedBrightnessYet = true;
+                // DJH: IDK why but this fixes bug of door/boundaries fading after fadeIn
+                fadeOut = false;
             }
         }
-        
-        if(fadeIn && newAlpha >= 1)
-        {
-            fadeIn = false;
-        }
-        
     }
 
     public void FadeOutThenDisable()
@@ -90,6 +112,12 @@ public class fadeable : MonoBehaviour
 
     public void EnableThenFadeIn()
     {
+        // DJH add code here to start the thing at a relatively low alpha/brightness
+        mat = this.GetComponent<Renderer>().material;
+        Color objectColor = mat.color;
+        float newAlpha = objectColor.a * 0.01f;
+        mat.color = new Color(objectColor.r, objectColor.g, objectColor.b, newAlpha);
+
         this.transform.gameObject.SetActive(true);
         Debug.Log("WE ARE SETTING THE FOLLOWING GAME OBJECT TO BE ACTIVE");
         Debug.Log(this.transform.gameObject.name);
